213e0f81afb35f3ec8ecf68875ad500a43ee1b91
soushi-ux <137749063+soushi-ux@users.noreply.github.com>
2023-06-28 13:57:36 +0900

    Add files via upload

--- assessment.js
+++ assessment.js
@@ -26,9 +26,9 @@ assessmentButton.onclick = () => {
   tweetDivision.innerText = '';
   const anchor = document.createElement('a');
   const hrefValue =
-  'https://twitter.com/intent/tweet?button_hashtag=' +
-  encodeURIComponent('あなたのいいところ') +
-  '&ref_src=twsrc%5Etfw';
+    'https://twitter.com/intent/tweet?button_hashtag=' +
+    encodeURIComponent('あなたのいいところ') +
+    '&ref_src=twsrc%5Etfw';
 
   anchor.setAttribute('href', hrefValue);
   anchor.setAttribute('class', 'twitter-hashtag-button');
@@ -42,29 +42,29 @@ assessmentButton.onclick = () => {
   tweetDivision.appendChild(script);
 };
 
-  userNameInput.onkeydown = event => {
-    if (event.key === 'Enter') {
-      assessmentButton.onclick();
-    }
-  };
+userNameInput.onkeydown = event => {
+  if (event.key === 'Enter') {
+    assessmentButton.onclick();
+  }
+};
 
-  const answers = [
-'###userName###のいいところは声です。###userName###の特徴的な声は皆を惹きつけ、心に残ります。' ,
-'###userName###のいいところはまなざしです。###userName###に見つめられた人は、気になって仕方がないでしょう。' ,
-'###userName###のいいところは情熱です。###userName###の情熱に周りの人は感化されます。' ,
-'###userName###のいいところは厳しさです。###userName###の厳しさがものごとをいつも成功に導きます。' ,
-'###userName###のいいところは知識です。博識な###userName###を多くの人が頼りにしています。' ,
-'###userName###のいいところはユニークさです。###userName###だけのその特徴が皆を楽しくさせます。' ,
-'###userName###のいいところは用心深さです。###userName###の洞察に、多くの人が助けられます。' ,
-'###userName###のいいところは見た目です。内側から溢れ出る###userName###の良さに皆が気を惹かれます。' ,
-'###userName###のいいところは決断力です。###userName###がする決断にいつも助けられる人がいます。' ,
-'###userName###のいいところは思いやりです。###userName###に気をかけてもらった多くの人が感謝しています。' ,
-'###userName###のいいところは感受性です。###userName###が感じたことに皆が共感し、わかりあうことができます。' ,
-'###userName###のいいところは節度です。強引すぎない###userName###の考えに皆が感謝しています。' ,
-'###userName###のいいところは好奇心です。新しいことに向かっていく###userName###の心構えが多くの人に魅力的に映ります。' ,
-'###userName###のいいところは気配りです。###userName###の配慮が多くの人を救っています。' ,
-'###userName###のいいところはその全てです。ありのままの###userName###自身がいいところなのです。' ,
-'###userName###のいいところは自制心です。やばいと思ったときにしっかりと衝動を抑えられる###userName###が皆から評価されています。' ,
+const answers = [
+  '###userName###のいいところは声です。###userName###の特徴的な声は皆を惹きつけ、心に残ります。',
+  '###userName###のいいところはまなざしです。###userName###に見つめられた人は、気になって仕方がないでしょう。',
+  '###userName###のいいところは情熱です。###userName###の情熱に周りの人は感化されます。',
+  '###userName###のいいところは厳しさです。###userName###の厳しさがものごとをいつも成功に導きます。',
+  '###userName###のいいところは知識です。博識な###userName###を多くの人が頼りにしています。',
+  '###userName###のいいところはユニークさです。###userName###だけのその特徴が皆を楽しくさせます。',
+  '###userName###のいいところは用心深さです。###userName###の洞察に、多くの人が助けられます。',
+  '###userName###のいいところは見た目です。内側から溢れ出る###userName###の良さに皆が気を惹かれます。',
+  '###userName###のいいところは決断力です。###userName###がする決断にいつも助けられる人がいます。',
+  '###userName###のいいところは思いやりです。###userName###に気をかけてもらった多くの人が感謝しています。',
+  '###userName###のいいところは感受性です。###userName###が感じたことに皆が共感し、わかりあうことができます。',
+  '###userName###のいいところは節度です。強引すぎない###userName###の考えに皆が感謝しています。',
+  '###userName###のいいところは好奇心です。新しいことに向かっていく###userName###の心構えが多くの人に魅力的に映ります。',
+  '###userName###のいいところは気配りです。###userName###の配慮が多くの人を救っています。',
+  '###userName###のいいところはその全てです。ありのままの###userName###自身がいいところなのです。',
+  '###userName###のいいところは自制心です。やばいと思ったときにしっかりと衝動を抑えられる###userName###が皆から評価されています。'
 ];
 
 /**
@@ -84,7 +84,7 @@ function assessment(userName) {
   let result = answers[index];
 
   result = result.replaceAll('###userName###', userName);
-    return result;
+  return result;
 }
 
 // テストコード
@@ -97,3 +97,4 @@ console.assert(
   assessment('太郎') === assessment('太郎'),
   '入力が同じ名前なら同じ診断結果を出力する処理が正しくありません。'
 );
+Please Type!
